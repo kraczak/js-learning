6bff9c930ce70733e0860fd1b1203dcf1f921fe7
kraczak <kornel.raczak@gmail.com>
2023-07-16 01:17:04 +0200

    added arrow functions examples

---
+++ arrowFunctions.js
@@ -0,0 +1,42 @@
+'use strict';
+
+
+// Arrow functions are a new way to write functions in ES6.
+// They are also called fat arrow functions
+// because they use a new token =>, which looks like a fat arrow.
+// Arrow functions are anonymous and change the way this binds in functions.
+
+let sum = (a, b) => a + b;
+
+alert(sum(1, 2)); // 3
+
+
+// with one argument parentheses can be omitted
+
+let double = n => n * 2;
+alert(double(3)); // 6
+
+// without arguments parentheses are required
+
+let sayHi = () => alert("Hello!");
+sayHi();
+
+// multiline arrow functions
+
+let sumV2 = (a, b) => {
+    return a + b; // if we use curly braces, then we need an explicit "return"
+}
+
+alert(sumV2(1, 2));
+
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask(
+    "Do you agree?",
+    () => { alert("You agreed."); },
+    () => { alert("You canceled the execution."); }
+);
