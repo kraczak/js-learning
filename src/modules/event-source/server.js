60c25fa8c9669f758210c202d66ec129342a9e5d
kraczak <kornel.raczak@gmail.com>
2023-09-23 21:58:58 +0200

    added EventSource client + server example

---
+++ src/modules/event-source/server.js
@@ -0,0 +1,42 @@
+let http = require('http');
+let url = require('url');
+let querystring = require('querystring');
+let nodeStatic = require('node-static');
+let fileServer = new nodeStatic.Server('.');
+
+function onDigits(req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/event-stream; charset=utf-8',
+        'Cache-Control': 'no-cache',
+        'Access-Control-Allow-Origin': '*'
+    });
+
+    let i = 0;
+    let timer = setInterval(write, 500);
+
+    function write() {
+        i++;
+        if (i === 4) {
+            res.write('event: bye\ndata: bye-bye\n\n');
+            clearInterval(timer);
+            res.end();
+            return;
+        }
+        res.write(`id: ${i}\ndata: ${i}\n\n`);
+    }
+
+}
+
+function accept(req, res) {
+    if (req.url === '/digits') {
+        console.log(req);
+        onDigits(req, res);
+        return;
+    }
+
+    fileServer.serve(req, res);
+}
+
+let port = 8080;
+console.log(`Starting server on http://localhost:${port}`)
+http.createServer(accept).listen(port);
